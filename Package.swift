871050e56a634ccc4dc801c186cd55ca2010152a
Vadym Markov <markov.vadym@gmail.com>
2016-05-24 22:55:08 +0200

    Add S4 as dependency

--- Package.swift
+++ Package.swift
@@ -6,5 +6,7 @@ let package = Package(
     Target(name: "Tests", dependencies: [.Target(name: "Flamingo")]),
   ],
   dependencies: [
+    //Standards package. Contains protocols for cross-project compatability.
+    .Package(url: "https://github.com/open-swift/S4.git", majorVersion: 0, minor: 6),
   ]
 )
