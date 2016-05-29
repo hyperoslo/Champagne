2c6e276bbd52ec6c7f31b62a4c6d36c11f960c79
Vadym Markov <markov.vadym@gmail.com>
2016-05-30 00:57:50 +0200

    Use new router in application

--- Sources/Flamingo/Application.swift
+++ Sources/Flamingo/Application.swift
@@ -1,6 +1,5 @@
 import Foundation
 import HTTPServer
-import Router
 
 #if os(Linux)
   import Glibc
@@ -10,7 +9,7 @@ public class Application {
 
   public static let version = "0.0.1"
 
-  var router: Router?
+  public var router: Router
   var running = false
 
   var middleware: [Middleware] = [
@@ -19,27 +18,14 @@ public class Application {
     ErrorMiddleware()
   ]
 
-  public init() {}
-
-  // MARK: - Routing
-
-  public func draw(build: (map: RouteMap) -> Void) {
-    guard !running else { return }
-    router = Router("/", middleware: ParametersMiddleware(),
-                    CookiesMiddleware(),
-                    ErrorMiddleware()) { map in
-                      map.fallback(responder: FileResponder())
-                      build(map: map)
-    }
+  public init(middleware: [Middleware] = []) {
+    self.middleware.append(contentsOf: middleware)
+    router = Router(middleware: middleware)
   }
 
   // MARK: - Server
 
   public func start() throws {
-    guard let router = router else {
-      return
-    }
-
     try Server(responder: router).start()
     running = true
   }
