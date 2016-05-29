0a1800165227432a510d46898a41d2412b901b78
Vadym Markov <markov.vadym@gmail.com>
2016-05-29 16:49:26 +0200

    Middleware to handle cookies

--- Sources/Flamingo/Middleware/CookiesMiddleware.swift
+++ Sources/Flamingo/Middleware/CookiesMiddleware.swift
@@ -1,21 +1,22 @@
-import S4
-
 public class CookiesMiddleware: Middleware {
 
-    public func respond(to request: Request, chainingTo next: Responder) throws -> Response {
-        var newRequest = request
+  public func respond(to request: Request, chainingTo next: Responder) throws -> Response {
+    var request = request
 
-        if let rawCookie = newRequest.headers["Cookie"].values.first {
-            let cookiePairs = rawCookie.split(separator: ";")
-            for cookie in cookiePairs {
-                let keyValue = cookie.split(separator: "=")
-                newRequest.cookies[keyValue[0]] = keyValue[1]
-            }
-        }
+    if let cookieString = request.headers["Cookie"].values.first {
+      let cookieItem = cookieString.split(separator: ";")
 
-        var response = try next.respond(to: newRequest)
-        response.headers["Set-Cookie"] = Header(response.cookies.map { "\($0)=\($1)" }.joined(separator: ";"))
-        return response
+      cookieItem.forEach {
+        let keyValue = $0.split(separator: "=")
+        request.cookies[keyValue[0]] = keyValue[1]
+      }
     }
 
+    var response = try next.respond(to: request)
+    let headerValue = response.cookies.map({ "\($0)=\($1)" }).joined(separator: ";")
+
+    response.headers["Set-Cookie"] = Header(headerValue)
+
+    return response
+  }
 }
