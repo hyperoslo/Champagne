e9eff3286b18bf937b13d57de3666555b9a30afa
Vadym Markov <markov.vadym@gmail.com>
2016-05-29 16:50:32 +0200

    Support stencil views

--- Sources/Flamingo/View.swift
+++ Sources/Flamingo/View.swift
@@ -1,36 +1,26 @@
 import Stencil
+import String
 import PathKit
 
 public protocol ViewRendering {
-    init(path: String, context: [String: Any]?)
-    func render() -> String
+  init(path: String, context: [String: Any])
+  func render() -> String
 }
 
 public struct StencilRenderer: ViewRendering {
-    var template: Template?
-    var context: [String: Any]?
+  let path: Path
+  let context: [String: Any]
 
-    public init(path: String, context: [String: Any]?) {
-        let defaultTemplateLoader = TemplateLoader(paths: [Path(Config.viewsDirectory)])
-        if context != nil {
-            self.context = context
-            self.context!["loader"] = defaultTemplateLoader
-        } else {
-            self.context = ["loader": defaultTemplateLoader]
-        }
+  public init(path: String, context: [String: Any] = [:]) {
+    self.context = context
+    self.path = Path(Config.viewsDirectory) + "\(path).html.stencil"
+  }
 
-        var templatePath = path.characters.split {$0 == "/"}.map(String.init)
-        let templateName = templatePath.removeLast()
-
-        let appPath = Path(Config.viewsDirectory)
-        let paths = [appPath + templatePath.joined(separator: "/")]
-        let templateLoader = TemplateLoader(paths: paths)
-        self.template = templateLoader.loadTemplate(name: templateName + ".html.stencil")
+  public func render() -> String {
+    do {
+      return try Template(path: path).render(context: Context(dictionary: context))
+    } catch {
+      return "Failed to render template \(error)"
     }
-
-    public func render() -> String {
-        guard template != nil else { return "Template Not Found" }
-        guard context != nil else { return try! template!.render() }
-        return try! template!.render(context: Context(dictionary: context!))
-    }
+  }
 }
