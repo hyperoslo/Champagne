6bac9b4d5250cc0d4f11969c61ae92992e716b15
Vadym Markov <markov.vadym@gmail.com>
2016-05-30 00:58:01 +0200

    Update example

--- Sources/Development/main.swift
+++ Sources/Development/main.swift
@@ -5,14 +5,14 @@ Config.viewsDirectory = "Sources/Development/Views"
 let app = Application()
 let controller = Controller()
 
-app.draw { map in
+app.router.draw { map in
   map.root(respond: controller.index)
   map.resources("users", controller: controller)
 
-  map.compose("api") { map in
+  map.namespace("api") { map in
     map.root(respond: controller.index)
-    map.resources("/users", controller: controller)
-    map.get("/log", respond: controller.index)
+    map.resources("users", controller: controller)
+    map.get("log", respond: controller.index)
   }
 }
 
