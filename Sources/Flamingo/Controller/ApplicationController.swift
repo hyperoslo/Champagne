369ef33447b79752ae530ba89c076110cfa8f3af
Vadym Markov <markov.vadym@gmail.com>
2016-05-30 00:55:49 +0200

    Move view rendering to application controller extension

--- Sources/Flamingo/Controller/ApplicationController.swift
+++ Sources/Flamingo/Controller/ApplicationController.swift
@@ -1,3 +1,13 @@
 public protocol ApplicationController {
   init()
 }
+
+// MARK: - Rendering
+
+public extension ApplicationController {
+
+  public func render(_ template: String, context: [String: Any] = [:]) -> Response {
+    let body = Config.ViewRenderer.init(path: template, context: context).render()
+    return Response(status: .ok, contentType: .html, body: body)
+  }
+}
