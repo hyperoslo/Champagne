51315080445061a645ec7ee7c514fe21f555cb4b
Vadym Markov <markov.vadym@gmail.com>
2016-05-29 16:46:49 +0200

    Make typealiases for S4

--- Sources/Flamingo/S4.swift
+++ Sources/Flamingo/S4.swift
@@ -1,9 +1,48 @@
-//
-//  S4.swift
-//  Flamingo
-//
-//  Created by Vadym Markov on 26/05/16.
-//
-//
+import S4
+import C7
+import Router
 
-import Foundation
+// MARK: - Basic
+
+public typealias Byte = C7.Byte
+public typealias Data = C7.Data
+public typealias Stream = C7.Stream
+public typealias URI = S4.URI
+public typealias DataConvertible = S4.DataConvertible
+
+// MARK: - Request
+
+public typealias Request = S4.Request
+
+extension Request {
+  public typealias Method = S4.Method
+  public typealias Body = S4.Body
+  public typealias Headers = S4.Headers
+}
+
+// MARK: - Response
+
+public typealias Response = S4.Response
+public typealias Responder = S4.Responder
+public typealias BasicResponder = S4.BasicResponder
+
+extension Response {
+  public typealias Status = S4.Status
+  public typealias Body = S4.Body
+  public typealias Headers = S4.Headers
+}
+
+// MARK: - Headers
+
+extension S4.Headers {
+  public typealias Values = S4.Header
+  public typealias Key = C7.CaseInsensitiveString
+}
+
+// MARK: - Middleware
+
+public typealias Middleware = S4.Middleware
+
+// MARK: - Router
+
+public typealias RouteMap = RouterBuilder
