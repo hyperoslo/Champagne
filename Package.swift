470b32e9caaa0aa0207f4ec86bf782a5237b477b
Vadym Markov <markov.vadym@gmail.com>
2016-05-30 00:54:56 +0200

    Remove Router dependency

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
   dependencies: [
     .Package(url: "https://github.com/open-swift/S4.git", majorVersion: 0, minor: 6),
     .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 7),
+    .Package(url: "https://github.com/Zewo/RegexRouteMatcher.git", majorVersion: 0, minor: 6),
     .Package(url: "https://github.com/necolt/Stencil.git", versions: Version(0,5,6)..<Version(1,0,0)),
-    .Package(url: "https://github.com/Zewo/Router.git", majorVersion: 0, minor: 6),
     .Package(url: "https://github.com/VeniceX/HTTPServer.git", versions: Version(0,5,0)..<Version(1,0,0))
   ]
 )
