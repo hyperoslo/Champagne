754130b83be7eb61c1df4f520b2e50a81bef7deb
Vadym Markov <markov.vadym@gmail.com>
2016-05-29 16:48:53 +0200

    Router extensions

--- Sources/Flamingo/Router/Router.swift
+++ Sources/Flamingo/Router/Router.swift
@@ -1,54 +1,16 @@
-import Foundation
-import S4
 @_exported import Router
-import PathKit
 
-extension Router {
+public extension RouteMap {
 
-    public static func create(build: (route: RouterBuilder) -> Void) -> Router {
-        return self.init(middleware: ParametersMiddleware(), CookiesMiddleware()) { route in
-            route.fallback { request in
-                if let staticFile = serveStaticFile(request: request) {
-                    return staticFile
-                } else {
-                    return Response(status: .notFound, body: "Route Not Found")
-                }
-            }
+  func root(middleware: Middleware..., responder: Responder) {
+    addRoute(method: .get, path: "", middleware: middleware, responder: responder)
+  }
 
-            build(route: route)
-        }
-    }
+  func root(middleware: Middleware..., respond: Respond) {
+    addRoute(method: .get, path: "", middleware: middleware, responder: BasicResponder(respond))
+  }
 
-    static func serveStaticFile(request: Request) -> Response? {
-        guard request.uri.path != "/" else { return nil }
-
-        let publicPath = Path(Config.publicDirectory)
-        guard publicPath.exists && publicPath.isDirectory else { return nil }
-
-        let filePath = publicPath + String(request.uri.path!.characters.dropFirst())
-        guard filePath.exists else { return nil }
-
-        guard filePath.isReadable else {
-            return Response(status: .notFound, body: "Can't Open File. Permission Denied")
-        }
-
-        do {
-            let contents = try filePath.read()
-            if let body = String(data: contents, encoding: NSUTF8StringEncoding) {
-                return Response(status: .ok, body: body)
-            }
-        } catch {
-            return Response(status: .notFound, body: "Error Reading From File")
-        }
-
-        return nil
-    }
-
-}
-
-extension RouterBuilder {
-
-  public func resources(name: String, middleware: Middleware..., controller: ResourceController) {
+  func resources(_ name: String, middleware: Middleware..., controller: ResourceController) {
     get(name, respond: controller.index)
     get(name + "/new", respond: controller.new)
     get(name + "/:id", respond: controller.show)
@@ -56,5 +18,10 @@ extension RouterBuilder {
     post(name, respond: controller.create)
     delete(name + "/:id", respond: controller.destroy)
     patch(name + "/:id", respond: controller.update)
+  }
+
+  func compose(_ path: String = "", build: (builder: RouteMap) -> Void) {
+    let router = Router(build: build)
+    compose(path, router: router)
   }
 }
