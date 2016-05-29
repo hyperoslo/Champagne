7ae86515b4632434db19165adf8b525a7555fa05
Vadym Markov <markov.vadym@gmail.com>
2016-05-29 16:51:18 +0200

    Refactor application

--- Sources/Flamingo/Application.swift
+++ Sources/Flamingo/Application.swift
@@ -1,6 +1,6 @@
 import Foundation
-import S4
 import HTTPServer
+import Router
 
 #if os(Linux)
   import Glibc
@@ -10,15 +10,37 @@ public class Application {
 
   public static let version = "0.0.1"
 
-  var router = Router.create { route in }
+  var router: Router?
+  var running = false
+
+  var middleware: [Middleware] = [
+    ParametersMiddleware(),
+    CookiesMiddleware(),
+    ErrorMiddleware()
+  ]
 
   public init() {}
 
-  public func route(build: (builder: RouterBuilder) -> Void) {
-    router = Router.create(build: build)
+  // MARK: - Routing
+
+  public func draw(build: (map: RouteMap) -> Void) {
+    guard !running else { return }
+    router = Router("/", middleware: ParametersMiddleware(),
+                    CookiesMiddleware(),
+                    ErrorMiddleware()) { map in
+                      map.fallback(responder: FileResponder())
+                      build(map: map)
+    }
   }
 
+  // MARK: - Server
+
   public func start() throws {
+    guard let router = router else {
+      return
+    }
+
     try Server(responder: router).start()
+    running = true
   }
 }
