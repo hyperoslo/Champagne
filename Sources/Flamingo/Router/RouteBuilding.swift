f3cf404c61bd20f10d77413df17c46087e7e6301
Vadym Markov <markov.vadym@gmail.com>
2016-06-01 01:16:44 +0200

    Handle leading and trailing slashes

--- Sources/Flamingo/Router/RouteBuilding.swift
+++ Sources/Flamingo/Router/RouteBuilding.swift
@@ -1,4 +1,4 @@
-import HTTP
+@_exported import HTTP
 
 /**
   Basic protocol for route building.
@@ -10,6 +10,12 @@ public protocol RouteBuilding {
   /// List of registerd routes.
   var routes: [Route] { get set }
 
+  /// Append leading slash to the route path
+  var appendLeadingSlash: Bool { get }
+
+  /// Append trailing slash to the route path
+  var appendTrailingSlash: Bool { get }
+
   /**
     Builds a route and adds responder as an action on the corresponding method.
 
@@ -18,7 +24,7 @@ public protocol RouteBuilding {
     - Parameter middleware: Route-specific middleware.
     - Parameter responder: The responder.
   */
-  func add(method: Method, path: String, middleware: [Middleware], responder: Responder)
+  func add(method: Request.Method, path: String, middleware: [Middleware], responder: Responder)
 
   /**
     Adds a fallback on a given path.
@@ -27,7 +33,7 @@ public protocol RouteBuilding {
     - Parameter middleware: Route-specific middleware.
     - Parameter responder: The responder.
   */
-  func fallback(_ path: String, middleware: [Middleware], responder: Responder)
+  func fallback(on path: String, middleware: [Middleware], responder: Responder)
 }
 
 // MARK: - Default implementations
@@ -103,11 +109,22 @@ extension RouteBuilding {
   /**
     Returns a route for a given path.
 
-    - Parameter path: The path of a route.
+    - Parameter relativePath: Relative route path.
     - Returns: The route.
   */
-  func route(on path: String) -> BasicRoute? {
+  func routeFor(relativePath: String) -> BasicRoute? {
+    let path = absolutePathFor(relativePath)
     return routes.filter({ $0.path == path }).first as? BasicRoute
+  }
+
+  /**
+    Returns a route for a given path.
+
+    - Parameter absolutePath: Absolute route path.
+    - Returns: The route.
+  */
+  func routeFor(absolutePath: String) -> BasicRoute? {
+    return routes.filter({ $0.path == absolutePath }).first as? BasicRoute
   }
 
   /**
@@ -116,8 +133,24 @@ extension RouteBuilding {
     - Parameter path: The path of a route.
     - Returns: The full path.
   */
-  func fullPath(with path: String) -> String {
+  func absolutePathFor(_ path: String) -> String {
+    let root = self.path
     let path = path.hasPrefix("/") ? String(path.characters.dropFirst()) : path
-    return self.path + "/" + path
+    let separator = root.hasSuffix("/") ? "" : "/"
+    var result = root + separator + path
+
+    if appendLeadingSlash && !result.hasPrefix("/") {
+      result = "/" + result
+    } else if !appendLeadingSlash && result.hasPrefix("/") {
+      result = String(result.characters.dropFirst())
+    }
+
+    if appendTrailingSlash && !result.hasSuffix("/") {
+      result = result + "/"
+    } else if !appendTrailingSlash && result.hasSuffix("/") {
+      result = String(result.characters.dropLast())
+    }
+
+    return root + separator + path
   }
 }
