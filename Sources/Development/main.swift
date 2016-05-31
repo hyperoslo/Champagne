f0b8c36e8c41ac775fe50a13b0adb2f44b10e389
Vadym Markov <markov.vadym@gmail.com>
2016-05-31 22:04:28 +0200

    Refactor routing and add documentation

--- Sources/Development/main.swift
+++ Sources/Development/main.swift
@@ -7,12 +7,12 @@ let controller = Controller()
 
 app.router.draw { map in
   map.root(respond: controller.index)
-  map.resources("users", controller: controller)
+  map.resources("users", controller: Controller.self)
 
   map.namespace("api") { map in
     map.fallback(respond: controller.index)
     map.root(respond: controller.index)
-    map.resources("users", controller: controller)
+    map.resources("users", controller: Controller.self)
     map.get("log", respond: controller.index)
   }
 }
