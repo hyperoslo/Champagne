32b550fdc52328754d69614c7d65caee59cb3db9
Vadym Markov <markov.vadym@gmail.com>
2016-06-16 23:41:07 +0200

    Introduce Route

---
+++ Sources/Flamingo/Router/Route.swift
@@ -0,0 +1,39 @@
+public protocol Route: Responder {
+  var path: String { get }
+  var actions: [Method: Responder] { get }
+  var fallback: Responder { get }
+}
+
+extension Route {
+
+  public var fallback: Responder {
+    return BasicResponder { _ in
+      Response(status: .methodNotAllowed)
+    }
+  }
+
+  public func respond(to request: Request) throws -> Response {
+    let action = actions[request.method] ?? fallback
+    return try action.respond(to: request)
+  }
+}
+
+public final class BasicRoute: Route {
+  public let path: String
+  public var actions: [Method: Responder]
+  public var fallback: Responder
+
+  public init(path: String, actions: [Method: Responder] = [:], fallback: Responder = BasicRoute.defaultFallback) {
+    self.path = path
+    self.actions = actions
+    self.fallback = fallback
+  }
+
+  public func addAction(method: Method, action: Responder) {
+    actions[method] = action
+  }
+
+  public static let defaultFallback = BasicResponder { _ in
+    Response(status: .methodNotAllowed)
+  }
+}
