d303cca9be63f74de6405e6d954460c184e419db
Vadym Markov <markov.vadym@gmail.com>
2016-06-24 23:24:02 +0200

    Add route tests

--- Tests/Flamingo/Middleware/PathParameterMiddlewareTests.swift
+++ Tests/Flamingo/Middleware/PathParameterMiddlewareTests.swift
@@ -9,13 +9,36 @@ class PathParametersMiddlewareTests: XCTestCase {
     ]
   }
 
+  var middleware: PathParametersMiddleware!
+  let parameters = ["version": "v1"]
+
   override func setUp() {
     super.setUp()
+    middleware = PathParametersMiddleware(parameters: parameters)
   }
 
   // MARK: - Tests
 
   func testInit() {
-    XCTAssertEqual(2, 2)
+    XCTAssertEqual(middleware.parameters, parameters)
+  }
+
+  func testRespond() {
+    let request = Request(
+      method: Method.get,
+      uri: URI(path: "/api/v1"),
+      body: Data("")
+    )
+
+    let responder: Responder = BasicResponder { request in
+      XCTAssertEqual(request.pathParameters, self.parameters)
+      return Response(status: .ok)
+    }
+
+    do {
+      try middleware.respond(to: request, chainingTo: responder)
+    } catch {
+      XCTFail("ParametersMiddleware throw an error: \(error)")
+    }
   }
 }
