280c929746b8a0350c40176174059b047abde0e4
Vadym Markov <markov.vadym@gmail.com>
2016-06-24 23:16:19 +0200

    Add route tests

--- Tests/Flamingo/Router/RouteMatcherTests.swift
+++ Tests/Flamingo/Router/RouteMatcherTests.swift
@@ -5,7 +5,8 @@ class RouteMatcherTests: XCTestCase {
 
   static var allTests: [(String, (RouteMatcherTests) -> () throws -> Void)] {
     return [
-      ("testInit", testInit)
+      ("testInitWithoutParameterKeys", testInitWithoutParameterKeys),
+      ("testInitWitParameterKeys", testInitWitParameterKeys)
     ]
   }
 
@@ -15,25 +16,43 @@ class RouteMatcherTests: XCTestCase {
 
   // MARK: - Tests
 
-  func testInit() {
-    XCTAssertEqual(2, 2)
+  func testInitWithoutParameterKeys() {
+    let matcher = RouteMatcher("/home/test")
+
+    XCTAssertNotNil(matcher.regex)
+    XCTAssertEqual(matcher.segments, [])
+  }
+
+  func testInitWitParameterKeys() {
+    let matcher = RouteMatcher("/api/:version")
+
+    XCTAssertNotNil(matcher.regex)
+    XCTAssertEqual(matcher.segments, ["version"])
   }
 
   func testMatch() {
-    var matcher = try! RouteMatcher(route: Route(path: "/home/test"))
+    var matcher = RouteMatcher("/home/test")
     XCTAssertTrue(matcher.matches("/home/test"))
     XCTAssertFalse(matcher.matches("/home/test/index"))
 
-    matcher = try! RouteMatcher(route: Route(path: "/api/:version"))
+    matcher = RouteMatcher("/api/:version")
     XCTAssertTrue(matcher.matches("/api/v1"))
     XCTAssertFalse(matcher.matches("/api/v1/v1"))
 
-    matcher = try! RouteMatcher(route: Route(path: "/birds/test"))
+    matcher = RouteMatcher("/birds/test")
     XCTAssertTrue(matcher.matches("/birds/test"))
     XCTAssertFalse(matcher.matches("/birds/index"))
 
-    matcher = try! RouteMatcher(route: Route(path: "/birds/:id/detail"))
+    matcher = RouteMatcher("/birds/:id/detail")
     XCTAssertTrue(matcher.matches("/birds/1/detail"))
     XCTAssertFalse(matcher.matches("/birds/1/logs"))
+  }
+
+  func testParameters() {
+    var matcher = RouteMatcher("/home/test")
+    XCTAssertTrue(matcher.parameters("/home/test").isEmpty)
+
+    matcher = RouteMatcher("/api/:version")
+    XCTAssertEqual(matcher.parameters("/api/v1"), ["version": "v1"])
   }
 }
--- Tests/Flamingo/Router/RouteTests.swift
+++ Tests/Flamingo/Router/RouteTests.swift
@@ -1,21 +1,80 @@
 import XCTest
 @testable import Flamingo
 
-class RouteTests: XCTestCase {
+class RouteTests: XCTestCase, TestResponding {
 
   static var allTests: [(String, (RouteTests) -> () throws -> Void)] {
     return [
-      ("testInit", testInit)
+      ("testInit", testInit),
+      ("testInitDefaults", testInitDefaults),
+      ("testAddAction", testAddAction),
+      ("testInitDefaults", testInitDefaults),
+      ("testRespondWithAction", testRespondWithAction),
+      ("testRespondWithoutAction", testRespondWithoutAction)
     ]
   }
 
+  var route: Route!
+
+  let fallback = BasicResponder { _ in
+    Response(status: .notFound)
+  }
+
+  let responder = BasicResponder { _ in
+    Response(status: .ok, body: Data(""))
+  }
+
+  let request = Request(method: .get, uri: URI(path: "/api/v1"))
+
   override func setUp() {
     super.setUp()
+    route = Route(path: "/api/:version")
   }
 
   // MARK: - Tests
 
   func testInit() {
-    XCTAssertEqual(2, 2)
+    route = Route(
+      path: "/api/:version",
+      actions: [Method.get: responder],
+      fallback: fallback)
+
+    XCTAssertEqual(route.path, "/api/:version")
+
+    respond(to: route.actions[Method.get], with: .ok)
+    respond(to: route.fallback, with: .notFound)
+  }
+
+  func testInitDefaults() {
+    route = Route(path: "/api/:version")
+
+    XCTAssertEqual(route.path, "/api/:version")
+    XCTAssertTrue(route.actions.isEmpty)
+
+    respond(to: route.fallback, with: .methodNotAllowed)
+  }
+
+  func testAddAction() {
+    XCTAssertTrue(route.actions.isEmpty)
+    route.addAction(method: .get, action: responder)
+    XCTAssertEqual(route.actions.count, 1)
+
+    respond(to: route.actions[Method.get], with: .ok)
+  }
+
+  func testRespondWithAction() {
+    route.addAction(method: .get, action: responder)
+
+    respond(
+      to: BasicResponder { _ in try! self.route.respond(to: self.request) },
+      with: .ok
+    )
+  }
+
+  func testRespondWithoutAction() {
+    respond(
+      to: BasicResponder { _ in try! self.route.respond(to: self.request) },
+      with: .methodNotAllowed
+    )
   }
 }
