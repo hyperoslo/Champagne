1e1d752905d07cf726a2fa4d5feda4c8d3798db3
Vadym Markov <markov.vadym@gmail.com>
2016-06-27 16:34:34 +0200

    Add documentation

--- Sources/Flamingo/Utilities/JSON.swift
+++ Sources/Flamingo/Utilities/JSON.swift
@@ -1,23 +1,49 @@
 import Jay
 
+/**
+  A basic protocol for types that could be inititalized with JSON.
+*/
 public protocol JSONInitializable {
+
+  /**
+    Creates a new instance of `Self`.
+
+    - Parameter json: JSON object.
+    - Throws: An error that occurs during initialization.
+  */
   init(json: JSON) throws
 }
 
+/**
+  A basic protocol for types that could be represented as a JSON.
+*/
 public protocol JSONRepresentable {
+
+  /// JSON object
   var json: JSON { get }
 }
 
+/**
+  A basic protocol for types that could be inititalized with JSON
+  and represented as a JSON.
+*/
 public protocol JSONConvertible: JSONInitializable, JSONRepresentable {}
 
 // MARK: - DataConvertible
 
 extension JSON: DataConvertible {
 
+  /**
+    Creates a new instance of `JSON` from `Data`.
+
+    - Parameter data: Data.
+    - Throws: An error that occurs during initialization from data.
+  */
   public init(data: Data) throws {
     self = try Jay().jsonFromData(data.bytes)
   }
 
+  /// Data from JSON
   public var data: Data {
     let jay = Jay(formatting: .minified)
 
@@ -33,6 +59,11 @@ extension JSON: DataConvertible {
 
 extension JSON: ArrayLiteralConvertible {
 
+  /**
+    Creates a new instance of `JSON` from `JSON` elements.
+
+    - Parameter elements: `JSON` elements.
+  */
   public init(arrayLiteral elements: JSON...) {
     self = .array(elements)
   }
@@ -40,6 +71,11 @@ extension JSON: ArrayLiteralConvertible {
 
 extension JSON: DictionaryLiteralConvertible {
 
+  /**
+    Creates a new instance of `JSON` from `JSON` dictionary elements.
+
+    - Parameter elements: `JSON` dictionary elements.
+  */
   public init(dictionaryLiteral elements: (String, JSON)...) {
     var dictionary = [String : JSON](minimumCapacity: elements.count)
 
@@ -53,6 +89,11 @@ extension JSON: DictionaryLiteralConvertible {
 
 extension JSON: NilLiteralConvertible {
 
+  /**
+    Creates a new instance of `JSON` from nil literal.
+
+    - Parameter value: Nil literal.
+  */
   public init(nilLiteral value: Void) {
     self = .null
   }
