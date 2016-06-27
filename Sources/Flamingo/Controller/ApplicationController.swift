90bd07b324767133bcc2a266576261e117052464
Vadym Markov <markov.vadym@gmail.com>
2016-06-27 20:12:31 +0200

    Implement redirect to method

--- Sources/Flamingo/Controller/ApplicationController.swift
+++ Sources/Flamingo/Controller/ApplicationController.swift
@@ -5,7 +5,7 @@ public protocol ApplicationController {
   init()
 }
 
-// MARK: - Rendering
+// MARK: - Responses
 
 public extension ApplicationController {
 
@@ -14,7 +14,7 @@ public extension ApplicationController {
 
     - Parameter template: The name of a template.
     - Parameter context: Context parameters to use in a template.
-    - Returns: A response.
+    - Returns: The response.
   */
   public func render(_ template: String, context: [String: Any] = [:]) -> Response {
     let body = Config.ViewRenderer.init(path: template, context: context).render()
@@ -25,7 +25,7 @@ public extension ApplicationController {
     Renders a JSON object.
 
     - Parameter json: JSON representable.
-    - Returns: A response.
+    - Returns: The response.
   */
   public func render(json: JSONRepresentable) -> Response {
     return Response(status: .ok, mime: .json, body: json.json)
@@ -35,7 +35,7 @@ public extension ApplicationController {
     Renders a JSON object.
 
     - Parameter data: Data representable.
-    - Returns: A response.
+    - Returns: The response.
   */
   public func render(data: DataRepresentable, mime: MimeType) -> Response {
     return Response(status: .ok, mime: mime, body: data)
@@ -46,12 +46,27 @@ public extension ApplicationController {
   /**
     Determines the desired response format from the HTTP Accept header
     and responds with first corresponding passed response if there is any.
+
+    - Parameter request: The request.
+    - Parameter responders: Dictionary with mime responders.
+
+    - Returns: The response.
   */
   public func respond(to request: Request, _ responders: MimeResponder) -> Response {
     let accepts = request.headers["Accept"]?.split(separator: ",") ?? []
 
     return responders.filter({ accepts.contains($0.0.rawValue) }).first?.1()
       ?? Response(status: .notAcceptable)
+  }
+
+  /**
+    Redirects to the given path
+
+    - Parameter path: The path.
+    - Returns: The response.
+  */
+  public func redirect(to path: String) -> Response {
+    return Response(status: .found, headers: ["Location": path])
   }
 }
 
