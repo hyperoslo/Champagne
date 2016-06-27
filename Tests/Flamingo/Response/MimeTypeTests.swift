c7d7ef7d11c7bf183d64710332d014014e11eab5
Vadym Markov <markov.vadym@gmail.com>
2016-06-27 09:38:39 +0200

    Add mime type tests

---
+++ Tests/Flamingo/Response/MimeTypeTests.swift
@@ -0,0 +1,42 @@
+import XCTest
+@testable import Flamingo
+
+class MimeTypeTests: XCTestCase {
+
+  static var allTests: [(String, (MimeTypeTests) -> () throws -> Void)] {
+    return [
+      ("testRawValues", testRawValues),
+    ]
+  }
+
+  // MARK: - Tests
+
+  func testRawValues() {
+    XCTAssertEqual(MimeType.html.rawValue, "text/html")
+    XCTAssertEqual(MimeType.json.rawValue, "application/json")
+    XCTAssertEqual(MimeType.html.rawValue, "text/html")
+    XCTAssertEqual(MimeType.text.rawValue, "text/plain")
+    XCTAssertEqual(MimeType.js.rawValue, "text/javascript")
+    XCTAssertEqual(MimeType.css.rawValue, "text/css")
+    XCTAssertEqual(MimeType.ics.rawValue, "text/calendar")
+    XCTAssertEqual(MimeType.csv.rawValue, "text/csv")
+    XCTAssertEqual(MimeType.vcf.rawValue, "text/vcard")
+    XCTAssertEqual(MimeType.png.rawValue, "image/png")
+    XCTAssertEqual(MimeType.jpeg.rawValue, "image/jpeg")
+    XCTAssertEqual(MimeType.gif.rawValue, "image/gif")
+    XCTAssertEqual(MimeType.bmp.rawValue, "image/bmp")
+    XCTAssertEqual(MimeType.tiff.rawValue, "image/tiff")
+    XCTAssertEqual(MimeType.svg.rawValue, "image/svg+xml")
+    XCTAssertEqual(MimeType.mpeg.rawValue, "video/mpeg")
+    XCTAssertEqual(MimeType.xml.rawValue, "application/xml")
+    XCTAssertEqual(MimeType.rss.rawValue, "application/rss+xml")
+    XCTAssertEqual(MimeType.atom.rawValue, "application/atom+xml")
+    XCTAssertEqual(MimeType.yaml.rawValue, "application/x-yaml")
+    XCTAssertEqual(MimeType.multipartForm.rawValue, "multipart/form-data")
+    XCTAssertEqual(MimeType.urlEncodedForm.rawValue, "application/x-www-form-urlencoded")
+    XCTAssertEqual(MimeType.json.rawValue, "application/json")
+    XCTAssertEqual(MimeType.pdf.rawValue, "application/pdf")
+    XCTAssertEqual(MimeType.zip.rawValue, "application/zip")
+    XCTAssertEqual(MimeType.gzip.rawValue, "application/gzip")
+  }
+}
