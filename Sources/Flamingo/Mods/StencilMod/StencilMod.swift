19c97718d438a2ad51758eccd0692057ab255186
Vadym Markov <markov.vadym@gmail.com>
2016-07-06 03:17:27 +0200

    Fix Stencil mod again

--- Sources/Flamingo/Mods/StencilMod/StencilMod.swift
+++ Sources/Flamingo/Mods/StencilMod/StencilMod.swift
@@ -3,10 +3,14 @@ public class StencilMod: Mod {
   public static var scheme = ModScheme(name: "Stencil")
   public let container: Container
 
+  /**
+  */
   public required init(container: Container) {
     self.container = container
   }
 
+  /**
+  */
   public func mount(on kernel: Kernel) {
     if let config = container.resolve(Config.self) {
       container.register(TemplateEngine.self) {
