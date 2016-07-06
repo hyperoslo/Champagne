f234abaf782c9df63dc64db1959fc1c661e3c42d
Vadym Markov <markov.vadym@gmail.com>
2016-07-06 02:23:54 +0200

    Use root in the config's init

--- Sources/Flamingo/Component/Config/Config.swift
+++ Sources/Flamingo/Component/Config/Config.swift
@@ -3,27 +3,33 @@
 */
 public class Config {
 
-  ///
+  /// Root directory.
+  public let root: String
+
+  /// Server host.
   public var host = "0.0.0.0"
 
-  ///
+  /// Server port.
   public var port = 8080
 
-  ///
+  /// Application environment.
   public var environment: Environment = Environment("development")
 
-  ///
-  public var root = "/"
-
-  ///
+  /// Mod schemes.
   var modSchemes = [ModScheme]()
 
-  ///
+  /// Kernel scheme.
   var kernelScheme = KernelScheme(name: "App")
 
   ///
   public var webDir: String {
     return absolutize(path: "Web")
+  }
+
+  /**
+  */
+  public init(root: String = "/") {
+    self.root = root
   }
 
   public func absolutize(path: String) -> String {
