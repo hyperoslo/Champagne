1f7e98b7fdd247e4a8a05f5ad07e48e548c0c023
Vadym Markov <markov.vadym@gmail.com>
2016-07-08 13:00:25 +0200

    Fix all the tests

--- Tests/Flamingo/Framework/Base/Bubble/BubbleTests.swift
+++ Tests/Flamingo/Framework/Base/Bubble/BubbleTests.swift
@@ -6,7 +6,6 @@ class BubbleTests: XCTestCase {
   static var allTests: [(String, (BubbleTests) -> () throws -> Void)] {
     return [
       ("testInit", testInit),
-      ("testDraw", testDraw)
     ]
   }
 
@@ -14,27 +13,5 @@ class BubbleTests: XCTestCase {
 
   func testInit() {
     XCTAssertTrue(true)
-  }
-
-  func testDraw() {
-    // router.collection.get("index", responder: responder)
-    // XCTAssertEqual(router.collection.routes.count, 1)
-    //
-    // router.draw { map in
-    //   XCTAssertTrue(self.router.collection.routes.isEmpty)
-    //
-    //   map.get("test", responder: self.responder)
-    //   map.post("test", responder: self.responder)
-    //   map.fallback(responder: self.failResponder)
-    // }
-    //
-    // XCTAssertEqual(router.collection.routeFor(relativePath: "test")?.path, "/test")
-    // XCTAssertEqual(router.collection.routes.count, 2)
-    // XCTAssertEqual(router.collection.routes.first?.actions.count, 2)
-    // XCTAssertEqual(router.collection.routes.last?.actions.count, 0)
-    //
-    // respond(to: router.collection.routes.first?.actions[.get], with: .ok)
-    // respond(to: router.collection.routes.first?.actions[.post], with: .ok)
-    // respond(to: router.collection.routes.last?.fallback, with: .notFound)
   }
 }
--- Tests/Flamingo/Framework/Routing/RouterTests.swift
+++ Tests/Flamingo/Framework/Routing/RouterTests.swift
@@ -6,6 +6,7 @@ class RouterTests: XCTestCase, TestResponding {
   static var allTests: [(String, (RouterTests) -> () throws -> Void)] {
     return [
       ("testInit", testInit),
+      ("testDraw", testDraw),
       ("testMatch", testMatch),
       ("testRespond", testRespond)
     ]
@@ -44,6 +45,28 @@ class RouterTests: XCTestCase, TestResponding {
   func testInit() {
     XCTAssertEqual(router.middleware.count, 2)
     XCTAssertTrue(router.collection.routes.isEmpty)
+  }
+
+  func testDraw() {
+    router.collection.get("index", responder: responder)
+    XCTAssertEqual(router.collection.routes.count, 1)
+
+    router.draw { map in
+      XCTAssertTrue(self.router.collection.routes.isEmpty)
+    
+      map.get("test", responder: self.responder)
+      map.post("test", responder: self.responder)
+      map.fallback(responder: self.failResponder)
+    }
+
+    XCTAssertEqual(router.collection.routeFor(relativePath: "test")?.path, "/test")
+    XCTAssertEqual(router.collection.routes.count, 2)
+    XCTAssertEqual(router.collection.routes.first?.actions.count, 2)
+    XCTAssertEqual(router.collection.routes.last?.actions.count, 0)
+
+    respond(to: router.collection.routes.first?.actions[.get], with: .ok)
+    respond(to: router.collection.routes.first?.actions[.post], with: .ok)
+    respond(to: router.collection.routes.last?.fallback, with: .notFound)
   }
 
   func testMatch() {
