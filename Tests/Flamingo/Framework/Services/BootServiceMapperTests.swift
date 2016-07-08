ce6d724bdf0029af477e260ed703718abe7cd584
Vadym Markov <markov.vadym@gmail.com>
2016-07-08 14:57:07 +0200

    Implement boot service tests

--- Tests/Flamingo/Framework/Services/BootServiceMapperTests.swift
+++ Tests/Flamingo/Framework/Services/BootServiceMapperTests.swift
@@ -5,13 +5,30 @@ class BootServiceMapperTests: XCTestCase {
 
   static var allTests: [(String, (BootServiceMapperTests) -> () throws -> Void)] {
     return [
-      ("testInit", testInit)
+      ("testAddServices", testAddServices)
     ]
+  }
+
+  let container = Container()
+  var service: BootServiceMapper!
+
+  override func setUp() {
+    super.setUp()
+
+    container.register(Config.self) {
+      return Config(root: Globals.root)
+    }
+
+    service = BootServiceMapper()
   }
 
   // MARK: - Tests
 
-  func testInit() {
-    XCTAssertTrue(true)
+  func testAddServices() {
+    service.addServices(to: container)
+
+    XCTAssertNotNil(container.resolve(AssetProvider.self))
+    XCTAssertTrue(container.resolve(Responder.self, tag: "fallback") is FallbackResponder)
+    XCTAssertEqual(container.factories.count, 3)
   }
 }
