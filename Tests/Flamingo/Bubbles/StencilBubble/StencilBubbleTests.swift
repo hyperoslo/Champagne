057113f71ec349f32acfc57a27931517aa7c9af6
Vadym Markov <markov.vadym@gmail.com>
2016-07-08 13:43:49 +0200

    Test Stencil bubble

--- Tests/Flamingo/Bubbles/StencilBubble/StencilBubbleTests.swift
+++ Tests/Flamingo/Bubbles/StencilBubble/StencilBubbleTests.swift
@@ -5,13 +5,33 @@ class StencilBubbleTests: XCTestCase {
 
   static var allTests: [(String, (StencilBubbleTests) -> () throws -> Void)] {
     return [
-      ("testInit", testInit)
+      ("testScheme", testScheme),
+      ("testMount", testMount)
     ]
+  }
+
+  let kernel = AppKernel()
+  let container = Container()
+  var bubble: Bubble!
+
+  override func setUp() {
+    super.setUp()
+
+    container.register(Config.self) {
+      return Config(root: Globals.root)
+    }
+
+    bubble = StencilBubble(container: container)
   }
 
   // MARK: - Tests
 
-  func testInit() {
-    XCTAssertTrue(true)
+  func testScheme() {
+    XCTAssertEqual(StencilBubble.scheme.name, "Stencil")
+  }
+
+  func testMount() {
+    bubble.mount(on: kernel)
+    XCTAssertTrue(container.resolve(TemplateEngine.self) is StencilEngine)
   }
 }
